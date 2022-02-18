d8b7a2510f114a2ac54120ee5deb5e695ddfc18e
aubundy <aubundy@gmail.com>
2022-02-18 09:34:17 -0600

    Add Google Maps support

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,6 +1,6 @@
 const { locations } = require("./mock");
 const url = require("url");
-// const functions = require("firebase-functions");
+const functions = require("firebase-functions");
 
 module.exports.geocodeRequest = (request, response, client) => {
   const { city, mock } = url.parse(request.url, true).query;
@@ -9,19 +9,19 @@ module.exports.geocodeRequest = (request, response, client) => {
     return response.json(locationMock);
   }
 
-  // client
-  //   .geocode({
-  //     params: {
-  //       address: city,
-  //       key: functions.config().google.key,
-  //     },
-  //     timeout: 1000,
-  //   })
-  //   .then((res) => {
-  //     return response.json(res.data);
-  //   })
-  //   .catch((e) => {
-  //     response.status(400);
-  //     return response.send(e.response.data.error_message);
-  //   });
+  client
+    .geocode({
+      params: {
+        address: city,
+        key: functions.config().google.key,
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      return response.json(res.data);
+    })
+    .catch((e) => {
+      response.status(400);
+      return response.send(e.response.data.error_message);
+    });
 };
